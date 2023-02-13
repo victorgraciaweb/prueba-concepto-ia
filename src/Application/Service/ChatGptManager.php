d74ef1dc614be42ca5a75dee690b64b45cabc0cc
Víctor Gracia <info@victorgraciaweb.com>
2023-02-13 16:57:12 +0100

    Changes in .gitignore

--- src/Application/Service/ChatGptManager.php
+++ src/Application/Service/ChatGptManager.php
@@ -2,10 +2,20 @@
 
 namespace App\Application\Service;
 
+use OpenAI;
+
 class ChatGptManager
 {
-    public function getAnswer(string $id): string
+    public function getAnswer(string $question): string
     {
+        $client = OpenAI::client($_ENV['API_KEY_OPENAI']);
+
+        $result = $client->completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => $question,
+        ]);
+
+        echo $result['choices'][0]['text']; // an open-source, widely-used, server-side scripting language.
         return "Hola";
     }
 }
